f3849f522cdcb6e682f1ab420f448649ad9c45ba
Sam Keddy <skeddy13@yahoo.com>
2022-04-17 11:49:42 -0400

    fixed debug class to show colission boxes for all tile types

--- js/_debug.js
+++ js/_debug.js
@@ -5,7 +5,7 @@ var debug = {
 	//show players collission box
 		let bounding = new PIXI.Graphics();
 		bounding.lineStyle(1, 0xff0000);
-		bounding.drawRect(0, 0, 16, 16); // x, y, width, height
+		bounding.drawCircle(8, 8, 7);
 		GAME.player.addChild(bounding);	
 
 	//target squares to show where player is pointing (used for movement)
@@ -19,19 +19,57 @@ var debug = {
 		GAME.currentMap.playerSquare.drawRect(0, 0, 16, 16); // x, y, width, height
 		GAME.level.addChild(GAME.currentMap.playerSquare);	
 
+	//show collision boxes
+		//let collisionBoxes;	
+		GAME.CollisionTypes.forEach(ct => {
+			ct.objects.forEach(o => {
+
+				let collisionBox = new PIXI.Graphics();
+				let isContainer = (o.constructor.prototype instanceof PIXI.Container);
+				
+				//x/y coordinates to place on map (only used if not a container, since then its just a child of the sprite)
+				let x = isContainer?0:o.x;
+				let y = isContainer?0:o.y;
+
+				//rectangle collisions
+				if (ct.type == 'rect') {
+					collisionBox.lineStyle(1, 0x0000ff);
+					collisionBox.drawRect(x, y, 16, 16); // x, y, width, height
+				}
+
+				//circle collisions
+				if (ct.type == 'circle') {
+					collisionBox.lineStyle(1, 0x0000ff);
+					collisionBox.drawCircle(x+8, y+8, 8); // x, y, radius
+				}
+				
+				//add as child to object (or just add to stage if no object)
+				if (isContainer)
+					o.addChild(collisionBox);
+				else {
+					GAME.level.addChild(collisionBox);
+					console.log('added generic colission for', ct.objectHolderName,x,y,isContainer?'isContainer':'notContainer')
+				}
+			});
+		});
+
+
+		// Loop through all of the objectsToCheck and see if any have a collision
+		//let collision = collisionBoxes.find(this.checkForCollision.bind(this));
 			
-	//overlays map data image onto map 
-		let mapDataSprite = PIXI.Sprite.from(mapDataImage);
-		mapDataSprite.scale.set(TILESIZE, TILESIZE);
-		mapDataSprite.blendMode = PIXI.BLEND_MODES.SCREEN;
-		GAME.level.addChild(mapDataSprite);
+	//overlays map data image onto map (BROKEN)
+		//let mapDataSprite = PIXI.Sprite.from(mapDataImage);
+		//mapDataSprite.scale.set(TILESIZE, TILESIZE);
+		//mapDataSprite.blendMode = PIXI.BLEND_MODES.SCREEN;
+		//GAME.level.addChild(mapDataSprite);
 	},
 	onLoop: ()=>{
 		if (!DEBUG) return;
 
-		GAME.currentMap.playerSquare.x = playerGridX * TILESIZE;
-		GAME.currentMap.playerSquare.y = playerGridY * TILESIZE;
-		GAME.currentMap.targetSquare.x = (playerGridX+hspeed) * TILESIZE;
-		GAME.currentMap.targetSquare.y = (playerGridY+vspeed) * TILESIZE;		
+		//shows which space the player is in / targetting (for smooshing) (BROKEN)
+		//GAME.currentMap.playerSquare.x = playerGridX * TILESIZE;
+		//GAME.currentMap.playerSquare.y = playerGridY * TILESIZE;
+		//GAME.currentMap.targetSquare.x = (playerGridX+hspeed) * TILESIZE;
+		//GAME.currentMap.targetSquare.y = (playerGridY+vspeed) * TILESIZE;		
 	}
 }
