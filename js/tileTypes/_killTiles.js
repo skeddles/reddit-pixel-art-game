e46d2f9868d29f2b9cc05f7cc3960a1ca3f43fbd
Sam Keddy <skeddy13@yahoo.com>
2022-04-25 12:46:40 -0400

    added life icons and dying system

--- js/tileTypes/_killTiles.js
+++ js/tileTypes/_killTiles.js
@@ -7,15 +7,29 @@ new TileType('killTiles', [255,0,0], {
 		h: TILESIZE/2,
 	}),
 	uiInit: ()=> {
-		//lives number text
-		GAME.ui.lives = new PIXI.Text('3',{fontFamily :"Press Start 2P", fontSize: 8, fill : 0xffffff, align : 'left'});
-		GAME.ui.addChild(GAME.ui.lives);
+		//lives counter
 		GAME.currentMap.lives = 3;
-	},
-	uiUpdate: ()=> {
-		GAME.ui.lives.text = GAME.currentMap.lives;
-	}
-	
+
+		//container that holds the life images
+		let container = new PIXI.Container();
+			container.x=1;
+			container.y=1;
+			GAME.ui.addChild(container);
+		
+		//gray hearts that show empty lives
+		let emptyLives = PIXI.Sprite.from('images/lives-empty.png');
+			container.addChild(emptyLives);
+		
+		//icon for 2 or more lives
+		GAME.ui.lifeIcon2 = PIXI.Sprite.from('images/lives.png');
+			GAME.ui.lifeIcon2.x = 10;	
+			container.addChild(GAME.ui.lifeIcon2);
+
+		//icon for 3 lives (full health)
+		GAME.ui.lifeIcon3 = PIXI.Sprite.from('images/lives.png');
+			GAME.ui.lifeIcon3.x = 20;
+			container.addChild(GAME.ui.lifeIcon3);
+	}	
 });
 
 new CollisionType('killTiles', 'rect', function (tile) {
@@ -27,6 +41,16 @@ new CollisionType('killTiles', 'rect', function (tile) {
 
 		//lose a life
 		GAME.currentMap.lives--;
+
+		//if the player has lost all lives, return them to the hub world
+		if (GAME.currentMap.lives == 0)
+			return loadHubWorld();
+
+		//reflect new life count on ui
+		if (GAME.currentMap.lives == 2)
+			GAME.ui.lifeIcon3.destroy()
+		if (GAME.currentMap.lives == 1)
+			GAME.ui.lifeIcon2.destroy()
 
 		//make sure player doesn't teleport when sent back on top of the entrance portal
 		GAME.playerInEntranceFlag = true;
